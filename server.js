873a1b84730ca70422cd38f7e942a92825011c3a
dsa66253 <dsa66253@yahoo.com.tw>
2022-10-14 12:47:38 +0800

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+// import guessRoute from './routes/guess'
+
+const app = express()
+
+// init middleware
+app.use(cors())
+
+// define routes
+app.use('/', (req, res)=>{
+    res.send("hello")
+})
+
+// define server
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
+})
